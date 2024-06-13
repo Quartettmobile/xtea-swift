cc52a98328b6665d1d14791e88406bfeb6f59a11
Tobias Klüpfel <tobias.kluepfel@quartett-mobile.de>
2024-06-13 18:40:49 +0200

    Added stub implementation and fake test

--- Tests/xtea-swiftTests/xtea_swiftTests.swift
+++ Tests/xtea-swiftTests/xtea_swiftTests.swift
@@ -2,11 +2,9 @@ import XCTest
 @testable import xtea
 
 final class xteaTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testEncryption() throws {
+        let result = xteaEncrypt(data: Data([1]), key: Data([1]))
+        
+        XCTAssertEqual(result, Data([1]))
     }
 }
