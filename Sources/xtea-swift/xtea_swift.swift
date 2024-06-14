721bd4be98384c6d0157046e21681f72a11f791a
Tobias Klüpfel <tobias.kluepfel@quartett-mobile.de>
2024-06-14 09:19:05 +0200

    Added data structures & reference algorithm

--- Sources/xtea-swift/xtea_swift.swift
+++ Sources/xtea-swift/xtea_swift.swift
@@ -1,5 +1,44 @@
 import Foundation
 
-func xteaEncrypt(data: Data, key:Data) -> Data {
+/*
+ 
+ Reference implementation from https://en.wikipedia.org/wiki/XTEA
+ 
+ 
+ /* take 64 bits of data in v[0] and v[1] and 128 bits of key[0] - key[3] */
+
+ void encipher(unsigned int num_rounds, uint32_t v[2], uint32_t const key[4]) {
+     unsigned int i;
+     uint32_t v0=v[0], v1=v[1], sum=0, delta=0x9E3779B9;
+     for (i=0; i < num_rounds; i++) {
+         v0 += (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
+         sum += delta;
+         v1 += (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum>>11) & 3]);
+     }
+     v[0]=v0; v[1]=v1;
+ }
+ */
+
+
+/// Encrypt
+///  @parameters:
+///  - data: the data to encrypt, 64 bits.
+///  - key: the key to use to encrypt `data`. 128 bits.
+public func xteaEncrypt(data: XTEA.Data, key: XTEA.Key) -> XTEA.Data {
     return data
+}
+
+public enum XTEA {
+    /// A 128 bit key, stored as 4 UInt32s
+    public struct Key {
+        let k0: UInt32
+        let k1: UInt32
+        let k2: UInt32
+        let k3: UInt32
+    }
+    
+    public struct Data: Equatable {
+        let v0: UInt32
+        let v1: UInt32
+    }
 }
--- Tests/xtea-swiftTests/xtea_swiftTests.swift
+++ Tests/xtea-swiftTests/xtea_swiftTests.swift
@@ -3,8 +3,8 @@ import XCTest
 
 final class xteaTests: XCTestCase {
     func testEncryption() throws {
-        let result = xteaEncrypt(data: Data([1]), key: Data([1]))
+        let result = xteaEncrypt(data: XTEA.Data(v0: 1, v1: 2), key: XTEA.Key(k0: 1, k1: 2, k2: 3, k3: 4))
         
-        XCTAssertEqual(result, Data([1]))
+        XCTAssertEqual(result, XTEA.Data(v0: 1, v1: 2))
     }
 }
